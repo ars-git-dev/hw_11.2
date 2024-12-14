0c204a863bc480732f1c0b348dfd1f55dad78221
Arsalan Shagdarov <shacd2000@gmail.com>
2024-12-14 21:43:29 +0700

    add capital_words function

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def capital_string(input_string):
     """Эта функция принимает на вход строку и возвращает ее со всеми заглавными буквами"""
     return input_string.upper()
+
+def capital_words(input_string):
+    """Эта функция принимает на вход строку и возвращает
+    заглавными первые буквы каждого слова в строке"""
+    return input_string.title()
