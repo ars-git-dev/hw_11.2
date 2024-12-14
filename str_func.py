fbab7fbca17b61594001f612b8adb185d152c337
Arsalan Shagdarov <shacd2000@gmail.com>
2024-12-14 22:11:01 +0700

    fix capital_string docstring

--- str_func.py
+++ str_func.py
@@ -1,5 +1,4 @@
 def capital_string(input_string):
-    """Эта функция принимает на вход строку и возвращает ее со всеми заглавными буквами"""
     return input_string.upper()
 
 def capital_words(input_string):
