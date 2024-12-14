84f38acb568bb0e56ca7d20ed6c1b3ce32aae6e2
ars-git-dev <shacd2000@gmail.com>
2024-12-14 22:34:23 +0700

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,4 +1,6 @@
 def capital_string(input_string):
+    """Эта функция принимает на вход строку и возвращает
+    заглавными все буквы в строке"""
     return input_string.upper()
 
 def capital_words(input_string):
